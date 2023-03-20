17e5daef9f64908d34bf0d27650af819e75ccd9e
gitonga bretton <gitongabretton@gmail.com>
2023-03-20 18:43:35 +0300

    get all properties

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './mongodb/connect.js';
 import userRouter from './routes/user.routes.js';
-import propertyRouter from './routes/property.routes';
+import propertyRouter from './routes/property.routes.js';
 
 
 dotenv.config();
