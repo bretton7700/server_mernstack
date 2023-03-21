6758a6d25d3bbb2174b3cbb3d9e753a5fd27e55b
gitonga bretton <gitongabretton@gmail.com>
2023-03-21 08:57:40 +0300

    property detail

--- controllers/property.controller.js
+++ controllers/property.controller.js
@@ -12,78 +12,88 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET
 })
 
-const getAllProperties =async(req,res) => {
-    const { _end,_order,_start, _sort,title_like="", propertyType ="" } = req.query;
-    const query= {};
+const getAllProperties = async (req, res) => {
+    const { _end, _order, _start, _sort, title_like = "", propertyType = "" } = req.query;
+    const query = {};
 
-    if(propertyType !== ''){
+    if (propertyType !== '') {
         query.propertyType = propertyType;
     }
 
-    if(title_like){
-        query.title = { $regex: title_like, $options: 'i'};
+    if (title_like) {
+        query.title = { $regex: title_like, $options: 'i' };
     }
 
     try {
 
         const count = await Property.countDocuments({ query });
         const properties = await Property
-        .find(query)
-        .limit(_end)
-        .skip(_start)
-        .sort({ [_sort]:_order})
+            .find(query)
+            .limit(_end)
+            .skip(_start)
+            .sort({ [_sort]: _order })
 
         res.header('x-total-count', count);
-        res.header('Access-Control-Expose-Headers','x-total-count');
-        
+        res.header('Access-Control-Expose-Headers', 'x-total-count');
+
 
 
         res.status(200).json(properties);
-        
+
     } catch (error) {
-        res.status(500).json({ message: error.message})
-        
+        res.status(500).json({ message: error.message })
+
     }
 };
 
-const getPropertyDetail = async(req,res) => {};
+const getPropertyDetail = async (req, res) => {
+
+    const { id } = req.params;
+    const propertyExists = await Property.findOne({ _id: id }).populate('creator');
+
+    if (propertyExists) { 
+        res.status(200).json(propertyExists);
+     }else{
+        res.status(404).json({ message: 'Property not found '})
+     }
+};
 
 
-const createProperty = async(req, res) => {
+const createProperty = async (req, res) => {
     try {
-      const { title, description, propertyType, location, price, photo, email } = req.body;
-  
-      const user = await User.findOne({ email });
-  
-      if (!user) throw new Error('User not found');
-  
-      const photoUrl = await cloudinary.uploader.upload(photo);
-  
-      const newProperty = await Property.create({
-        title,
-        description,
-        propertyType,
-        location,
-        price,
-        photo: photoUrl.url,
-        creator: user._id,
-      });
-  
-      user.allProperties.push(newProperty._id);
-      await user.save();
-  
-      res.status(200).json({ message: 'property created successfully' });
+        const { title, description, propertyType, location, price, photo, email } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if (!user) throw new Error('User not found');
+
+        const photoUrl = await cloudinary.uploader.upload(photo);
+
+        const newProperty = await Property.create({
+            title,
+            description,
+            propertyType,
+            location,
+            price,
+            photo: photoUrl.url,
+            creator: user._id,
+        });
+
+        user.allProperties.push(newProperty._id);
+        await user.save();
+
+        res.status(200).json({ message: 'property created successfully' });
     } catch (error) {
-      res.status(500).json({ message: error.message });
+        res.status(500).json({ message: error.message });
     }
-  };
-  
+};
 
 
-const updateProperty = async (req,res) => {};
+
+const updateProperty = async (req, res) => { };
 
 
-const deleteProperty = async(req,res) => {};
+const deleteProperty = async (req, res) => { };
 
 
 export {
