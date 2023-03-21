b8bdfef0a0fb9268dde78cad2ec2bb56507a2b02
gitonga bretton <gitongabretton@gmail.com>
2023-03-21 14:24:38 +0300

    agent controller

--- controllers/property.controller.js
+++ controllers/property.controller.js
@@ -90,10 +90,44 @@ const createProperty = async (req, res) => {
 
 
 
-const updateProperty = async (req, res) => { };
+const updateProperty = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { title, description, propertyType, location, price, photo} = req.body;
+
+        const photoUrl = await cloudinary.uploader.upload(photo);
+
+        await Property.findByIdAndUpdate({ _id: id}, {
+            title,
+            description,
+            propertyType,
+            location,
+            price,
+            photo: photoUrl.url || photo
+        })
+
+        res.status(200).json({ message: 'Property updated successfully'})
+    } catch (error) {
+        res.status(500).json({ message: error.message})
+    }
+ };
 
 
-const deleteProperty = async (req, res) => { };
+const deleteProperty = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const propertyToDelete = await Property.findOneAndDelete({ _id: id }).populate('creator');
+      if (!propertyToDelete) throw new Error('Property not Found');
+  
+      await propertyToDelete.creator.allProperties.pull(propertyToDelete);
+      await propertyToDelete.creator.save();
+  
+      res.status(200).json({ message: 'Property deleted Successfully' });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  };
+  
 
 
 export {
